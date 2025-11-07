037b3fab40dcdd33cc5ae123a2d50cbf9046142a
Enjim <onimsarker@gmail.com>
2025-11-07 23:24:27 +0100

    Remove run_app.bat as part of the cleanup for a streamlined setup process.

--- run_app.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo TravelRag - Starting the app...
-echo.
-echo If this is your first time, run: python setup.py
-echo.
-echo Starting Streamlit app...
-streamlit run streamlit_app.py
-pause
